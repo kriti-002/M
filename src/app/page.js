9848882880b6215176a9c8e6bf5d08e58e76bcd0
Pranav Kushawaha <pranavkushwaha506@gmail.com>
2024-11-24 11:58:39 +0530

    mera commit

--- src/app/page.js
+++ src/app/page.js
@@ -6,17 +6,17 @@ import MovieCard from "./components/MovieCard";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-50">
       <Header />
       <div className="w-full flex flex-col justify-center items-center">
         <h1 className="text-4xl">Welcome to MovieMania!</h1>
         <p className="font-light">Your one-stop destination for movies and reviews.</p>
       </div>
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
+      <div className="flex flex-col gap-2 justify-center items-center">
+
+        {[1,2,3,4].map((item,index)=>(
+          <MovieCard number={item} key={index}/>
+        ))}
       </div>
       <Footer />
     </div>
